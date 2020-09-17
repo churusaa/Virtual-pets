160c9cc171e86bb5840a35b0f9b5289665e41f25
Millicent Duke <millicent.j.duke@gmail.com>
2020-09-17 10:19:17 -0400

    Added default thirst and settable thirst

--- src/test/java/virtual_pet/VirtualPet.java
+++ src/test/java/virtual_pet/VirtualPet.java
@@ -1,22 +1,26 @@
 package virtual_pet;
 
 
-
 public class VirtualPet {
 
     private String name;
     private int hunger;
+    private int thirst;
 
-    public VirtualPet(String name, int hunger) {
+    public VirtualPet(String name, int hunger, int thirst) {
         this.name = name;
         this.hunger = hunger;
+        this.thirst = thirst;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public int getHunger() {
         return hunger;
     }
+
+    public int getThirst() {
+        return thirst; }
 }
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class VirtualPetTest {
     @Test
     public void shouldBeAbleToCreatePet() {
-        VirtualPet pet = new VirtualPet("Test Name" , 12);
+        VirtualPet pet = new VirtualPet("Test Name", 12, 10);
     }
 
     @Test
     public void petShouldHaveName() {
         //arrangement
-        VirtualPet underTest = new VirtualPet("Jeff" , 15);
+        VirtualPet underTest = new VirtualPet("Jeff", 15, 14);
 
         //action
         String result = underTest.getName();
@@ -24,14 +24,27 @@ public class VirtualPetTest {
 
     @Test
     public void shouldHaveDefaultHunger() {
-        VirtualPet underTest = new VirtualPet("Steve", 10);
+        VirtualPet underTest = new VirtualPet("Steve", 10, 1);
 
         int expected = underTest.getHunger();
 
         assertEquals(expected, 10);
     }
 
-    //@Test
+    @Test
+    public void shouldHaveDefaultThirst() {
+        VirtualPet underTest = new VirtualPet("Brenda", 10, 8);
+
+        int expected = underTest.getThirst();
+
+        assertEquals(expected, 8);
+    }
+
+    //TODO add Boredom
+
+    //TODO add Playfulness
+
+    //TODO add Energy
 
 
 }
